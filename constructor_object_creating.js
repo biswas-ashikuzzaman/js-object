6f410d4fe74109ee0bfb675db8e87ebd661ff704
Ashikuzzaman Biswas <biswasashikuzzaman@gmail.com>
2025-05-27 10:46:48 +0600

    update

---
+++ constructor_object_creating.js
@@ -0,0 +1,13 @@
+function Person (n,a,i) {
+    this.name=n;
+    this.age= a;
+    this.id= i;
+
+}
+const sifat = new Person("Sifat",25, 34);
+const asif = new Person("Asif", 45,44);
+const ashik = new Person("Ashik",22,1288603);
+// console.log(sifat.name + " "+ sifat.age + " "+ sifat.id);
+console.log(asif);
+console.log(sifat);
+console.log(ashik)
